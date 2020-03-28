553fa54e8cb1c61957e2ba77abe0fc5a227923d0
coin4646 <coin46@naver.com>
2020-03-28 20:12:51 +0900

    해더 수정

--- homework/scrapinghomework.py
+++ homework/scrapinghomework.py
@@ -2,12 +2,11 @@ import requests
 from bs4 import BeautifulSoup
 
 # URL을 읽어서 HTML를 받아오고,
-headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-data = requests.get('https://www.genie.co.kr/chart/top200?ditc=D&rtm=N&ymd=20200309', headers)
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+data = requests.get('https://www.genie.co.kr/chart/top200?ditc=D&rtm=N&ymd=20200309', headers=headers)
 
 
 soup = BeautifulSoup(data.text, 'html.parser')
-print(soup)
 
 
 songs = soup.select('#body-content > div.newest-list > div > table > tbody > tr')
@@ -17,4 +16,4 @@ for song in songs:
     title = song.select_one('td.info > a.title.ellipsis')
     singer = song.select_one('td.info > a.artist.ellipsis')
     if title is not None:
-        print (rank, title.text, singer.text)
+        print (rank.text, title.text, singer.text)
